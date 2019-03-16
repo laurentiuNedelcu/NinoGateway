a8c31268416bd856b783f20ead9bcd6e25d92291
joaquimYuste <yuste321@gmail.com>
2019-03-16 18:25:17 +0100

    Main menu

---
+++ app/src/main/java/com/example/ninosproject/ModeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ninosproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ModeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mode_seleccion)
+    }
+}
