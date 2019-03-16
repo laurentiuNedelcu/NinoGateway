8be3de2aba691aebb35ed458aed75e2d8e52fb71
joaquimYuste <yuste321@gmail.com>
2019-03-16 19:38:33 +0100

    Menu de selecció de mode afegit

--- app/src/main/java/com/example/ninosproject/MainActivity.kt
+++ app/src/main/java/com/example/ninosproject/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.ninosproject
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+        var button_jugar : Button = findViewById<Button>(R.id.jugar)
+        var button_exit : Button = findViewById<Button>(R.id.button_exit)
+
+        button_jugar.setOnClickListener {
+            val intent: Intent = Intent(this, ModeActivity::class.java)
+            startActivity(intent);
+        }
+
+        button_exit.setOnClickListener {
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/ninosproject/ModeActivity.kt
+++ app/src/main/java/com/example/ninosproject/ModeActivity.kt
@@ -2,11 +2,15 @@ package com.example.ninosproject
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ModeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mode_seleccion)
+        val back_button : Button = findViewById<Button>(R.id.button_back)
+
+        back_button.setOnClickListener { finish() }
     }
 }
