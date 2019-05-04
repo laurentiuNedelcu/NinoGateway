d497142c0762cfaff80a86b649461243a4a1c07f
joaquimYuste <yuste321@gmail.com>
2019-05-04 19:22:00 +0200

    Basic loop en el GameThread

--- app/src/main/java/com/example/ninosproject/Logic/GameThread.kt
+++ app/src/main/java/com/example/ninosproject/Logic/GameThread.kt
@@ -1,3 +1,75 @@
 package com.example.ninosproject.Logic
 
-class GameThread
+import android.graphics.Canvas
+import android.view.SurfaceHolder
+import java.lang.Exception
+
+class GameThread(surfaceHolder: SurfaceHolder, gameView: GameView) : Thread() {
+    private val FPS : Long = 30;
+    private var avgFPS : Double = 0.0;
+    private var holder: SurfaceHolder;
+    private var gameView: GameView;
+    private var isRunning: Boolean = false;
+
+    companion object { //Para poder llamar al objeto estaticamente (ex: GameThread.canvas)
+        var canvas: Canvas? = null;
+    }
+
+    init {
+        this.holder = surfaceHolder;
+        this.gameView = gameView;
+        isRunning = true;
+    }
+
+    override fun run(){
+        var startTime: Long;
+        var timeMilis: Long;
+        var waitMilis: Long;
+        var totalTime: Long = 0;
+        var frameCount: Long = 0;
+        var targetTime: Long = 1000.div(FPS).toLong();
+
+        while(isRunning){
+            startTime = System.nanoTime();
+            canvas = null;
+            try{
+                canvas = this.holder.lockCanvas();
+                synchronized(holder){ //Este es el loop del juego, lo demas es para prevenir errores
+                    //this.gameView.update(); //implementarlo
+                    //this.gameView.draw(canvas) //implementarlo
+                }
+            }catch (e: Exception){
+                e.printStackTrace();
+            }finally {
+                if(canvas!=null){
+                    try{
+                        holder.unlockCanvasAndPost(canvas);
+                    }catch (e: Exception){
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            timeMilis = (System.nanoTime().minus(startTime)).div(1000000);
+            waitMilis = targetTime.minus(timeMilis);
+
+            try{
+                sleep(waitMilis);
+            }catch (e: Exception){
+                e.printStackTrace();
+            }
+
+            totalTime += System.nanoTime().minus(startTime);
+            frameCount++;
+            if(frameCount == FPS){
+                avgFPS = (100/((totalTime/frameCount)/1000000)).toDouble();
+                frameCount = 0;
+                totalTime = 0;
+                System.out.println()
+            }
+        }
+    }
+    public fun setRunning(isRunning: Boolean){
+        this.isRunning = isRunning;
+    }
+}
