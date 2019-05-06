5f983d6e22939a948dc34cb8a2ab511786cb97a1
laurentiuNedelcu <31955816+laurentiuNedelcu@users.noreply.github.com>
2019-05-06 16:54:19 +0200

    Añadir los botones a la partida

--- app/src/main/java/com/example/ninosproject/Activities/PlayActivity.kt
+++ app/src/main/java/com/example/ninosproject/Activities/PlayActivity.kt
@@ -1,5 +1,6 @@
 package com.example.ninosproject.Activities
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.content.res.Resources
@@ -10,7 +11,9 @@ import android.view.LayoutInflater
 import android.view.Window
 import android.view.WindowManager
 import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.PopupWindow
+import android.widget.RelativeLayout
 import com.example.ninosproject.Logic.GameView
 import com.example.ninosproject.R
 
@@ -21,14 +24,69 @@ class PlayActivity : AppCompatActivity() {
     //inicialitzem la finestra
     private lateinit var popupWindow : PopupWindow
     private lateinit var pause : Button
-
     private var isPopupOn : Boolean = false
 
+    //Prova per al cercle
+    private lateinit var gameView: GameView
+    private lateinit var game: FrameLayout //Contindra la vista del gameView y de los botones
+    private lateinit var gameButtons: RelativeLayout
+
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //**********************************PROVA**************************************************
+        gameView = GameView(this)
+        game = FrameLayout(this)
+        gameButtons = RelativeLayout(this)
+
+        //Creamos los botones
+        val buttonRight: Button = Button(this)
+        buttonRight.text = "->"
+        buttonRight.id = 123456 //Hay que ponerles un id diferente para diferenciarlos de los default buttons (que tienen el mismo id)
+
+        val buttonLeft: Button = Button(this)
+        buttonLeft.text = "<-"
+        buttonLeft.id = 654321
+
+        //Definimos los parametros de diseño de los botones para envolverlos en el layout
+        val layoutButtonRight: RelativeLayout.LayoutParams = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
+            RelativeLayout.LayoutParams.WRAP_CONTENT)
+
+        val layoutButtonLeft: RelativeLayout.LayoutParams = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
+            RelativeLayout.LayoutParams.WRAP_CONTENT)
+
+        //Cuando tengamos los layouts de los botones hacemos otro para envolver a estos dos layouts
+        val params: RelativeLayout.LayoutParams = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,
+            RelativeLayout.LayoutParams.MATCH_PARENT)
+
+        //Ahora le ponemos este layout al RelativeLayout (que lo he anomenado gameButtons)
+        gameButtons.layoutParams = params
+        //Y le añadimos los botones
+        gameButtons.addView(buttonRight)
+        gameButtons.addView(buttonLeft)
+
+        //Hasta ahora tenemos creado los layouts, y ahora decidimos donde queremos ponerlos
+        layoutButtonLeft.addRule(RelativeLayout.ALIGN_PARENT_LEFT)
+        layoutButtonLeft.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM)
+        buttonLeft.layoutParams = layoutButtonLeft
+
+        layoutButtonRight.addRule(RelativeLayout.ALIGN_PARENT_RIGHT)
+        layoutButtonRight.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM)
+        buttonRight.layoutParams = layoutButtonRight
+
+        //**********************************PROVA**************************************************
+
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
-        setContentView(GameView(this))
+
+        //PROVA
+        //Como he dicho antes el FrameLayout va a contener las dos "vistas"
+        game.addView(gameView)
+        game.addView(gameButtons)
+        setContentView(game)
+
+        //setContentView(GameView(this))
 
 /*        val tmp_win: Button = findViewById(R.id.tmpwinbutton)
         val tmp_lose: Button = findViewById(R.id.tmplosebutton)
