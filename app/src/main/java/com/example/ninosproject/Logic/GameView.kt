1a6851ac8b2797db5fb0a0342108d3aa5d2df920
Abdelkarim Azzouguagh Ouniri <32040687+abdelkarimazzouguagh@users.noreply.github.com>
2019-05-07 20:26:51 +0200

    level test

--- app/src/main/java/com/example/ninosproject/Logic/GameView.kt
+++ app/src/main/java/com/example/ninosproject/Logic/GameView.kt
@@ -1,13 +1,11 @@
 package com.example.ninosproject.Logic
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.Canvas
+import android.graphics.*
+import android.util.DisplayMetrics
 import android.view.SurfaceHolder
 import android.view.SurfaceView
 import android.widget.Button
-import com.example.ninosproject.Logic.GameThread.Companion.canvas
 import com.example.ninosproject.R
 
 class GameView(context: Context): SurfaceView(context), SurfaceHolder.Callback{
@@ -16,6 +14,9 @@ class GameView(context: Context): SurfaceView(context), SurfaceHolder.Callback{
     private var circle: Bitmap
     private var px: Float
     private var py: Float
+    private var screenW: Int
+    private var blockSize: Int
+    private var paint: Paint
     init {
         holder.addCallback(this)
         thread = GameThread(holder,this)
@@ -23,6 +24,15 @@ class GameView(context: Context): SurfaceView(context), SurfaceHolder.Callback{
         px = 0.0F
         py = 0.0F
         isFocusable = true
+        /*****************************************/
+        val matrix: DisplayMetrics = resources.displayMetrics
+        screenW = matrix.widthPixels
+        blockSize = screenW.div(17)
+        blockSize = (blockSize.div(5)).times(5)
+        paint = Paint()
+        paint.color = Color.WHITE
+
+
     }
 
     override fun surfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {
@@ -49,6 +59,7 @@ class GameView(context: Context): SurfaceView(context), SurfaceHolder.Callback{
         super.draw(canvas)
 
         canvas?.drawBitmap(circle,px,py,null)
+        drawMap(canvas!!)
     }
 
     fun updateR(){
@@ -62,5 +73,58 @@ class GameView(context: Context): SurfaceView(context), SurfaceHolder.Callback{
     fun startGame(){
         thread.start()
     }
+
+    private fun drawMap(canvas: Canvas) {
+        //paint.setColor(Color.BLUE)
+        //paint.setStrokeWidth(2.5f)
+        paint.strokeWidth = 2.5f
+        var x: Int
+        var y: Int
+        for (i in 0..17) {
+            for (j in 0..16) {
+                x = j * blockSize
+                y = i * blockSize
+                if (level[i][j] and 1 != 0)
+                // draws left
+                    canvas.drawLine(x.toFloat(), y.toFloat(), x.toFloat(), (y + blockSize - 1).toFloat(), paint)
+
+                if (level[i][j] and 2 != 0)
+                // draws top
+                    canvas.drawLine(x.toFloat(), y.toFloat(), (x + blockSize - 1).toFloat(), y.toFloat(), paint)
+
+                if (level[i][j] and 4 != 0)
+                // draws right
+                    canvas.drawLine(
+                        (x + blockSize).toFloat(),
+                        y.toFloat(),
+                        (x + blockSize).toFloat(),
+                        (y + blockSize - 1).toFloat(),
+                        paint
+                    )
+
+                if (level[i][j] and 8 != 0)
+                // draws bottom
+                    canvas.drawLine(
+                        x.toFloat(),
+                        (y + blockSize).toFloat(),
+                        (x + blockSize - 1).toFloat(),
+                        (y + blockSize).toFloat(),
+                        paint
+                    )
+            }
+        }
+        paint.color = Color.WHITE
+    }
+
+    internal val level = arrayOf(
+        intArrayOf(19, 26, 26, 18, 26, 26, 26, 26, 26, 26, 26, 26, 26, 18, 26, 26, 22),
+        intArrayOf(21, 0, 0, 21, 0, 0, 0, 21, 0, 0, 0, 0, 0, 21, 0, 0, 21),
+        intArrayOf(1, 26, 26, 16, 26, 18, 26, 24, 26, 24, 26, 18, 26, 16, 26, 26, 20),
+        intArrayOf(21, 26, 26, 20, 0, 25, 26, 22, 0, 19, 26, 28, 0, 17, 26, 26, 21),
+        intArrayOf(1, 0, 0, 21, 0, 0, 0, 21, 0, 21, 0, 0, 0, 21, 0, 0, 20),
+        intArrayOf(21, 0, 0, 21, 0, 19, 26, 24, 26, 24, 26, 22, 0, 21, 0, 0, 20),
+        intArrayOf(21, 0, 26, 16, 26, 20, 0, 0, 0, 0, 0, 17, 26, 16, 26, 26, 21),
+        intArrayOf(25, 26, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 26, 28)
+    )
 
 }
